29a8ec7ae60300ad56b08874e9a78bc36343d2ff
LuisHenriqueOliveira <52217846+LuisHenriqueOliveira@users.noreply.github.com>
2020-06-02 16:41:50 -0300

    Classe Teste

--- Teste.java
+++ Teste.java
@@ -1,27 +1,25 @@
 import javax.swing.*;
-import javax.swing.table.AbstractTableModel;
-import java.util.List;
-import java.awt.GridLayout;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
-
-public class Teste {
-   public static void main (String [] args){     
-      PessoasEsportes ped = new PessoasEsportes();
+import java.util.*;
+import javax.swing.table.*;
+public class Teste{
    
-      JTable table = new TableModel();
-      JScrollPane scrollPane = new JScrollPane(table);
-   
-      JFrame frame = new JFrame();
-      frame.getContentPane().add(scrollPane); 
-      frame.pack();
-      frame.setVisible(true);
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      frame.setLocationRelativeTo(null);
-   
-   
-   
+   public static void main(String []args){
+      String[] colunas={"nome","idade","sexo","endereco","vegetariano(a)","esporte"};
+      ArrayList<Pessoa> pessoas;
+      
+      PessoaFactory y= new PessoaFactory();
+      
+      pessoas=y.completaTabela();
+      
+      TableModel modelo = new TableModel(pessoas,colunas);
+      
+      JTable tabela= new JTable(modelo);
+      JScrollPane rolagem= new JScrollPane(tabela);
+      JFrame quadro= new JFrame(); 
+      quadro.getContentPane().add(rolagem);
+      quadro.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      quadro.setLocationRelativeTo(null);
+      quadro.pack();
+      quadro.setVisible(true);
    }
 }
